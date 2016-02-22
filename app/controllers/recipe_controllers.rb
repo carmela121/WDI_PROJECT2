16574ba0d0516cc656d97f502a7fe359194cddcc
carmela121 <mcarmencarmona@hotmail.com>
2016-02-22 14:50:36 +0000

    Recipe restful routes

---
+++ app/controllers/recipe_controllers.rb
@@ -0,0 +1,50 @@
+# INDEX
+get '/recipes' do 
+  @recipe = Recipe.all 
+  erb :'recipe/index'
+end
+
+# NEW
+
+get '/recipe/new' do 
+  @recipe = Recipe.new
+  erb :'recipes/new'
+end
+
+# CREATE
+
+post '/recipes' do 
+  @recipe = Recipe.new(params[:recipe])
+  if @recipe.save
+    redirect "/recipes"
+  else
+    erb :"recipes/new"
+  end
+end
+
+#SHOW
+
+get '/recipes/:id' do 
+  @recipe = Recipe.find(params[:id])
+  if @recipe
+  erb :'recipes/show'
+  else
+  redirect "/recipes"
+  end
+end
+
+# EDIT
+get "/recipes/:id/edit" do
+  @recipe = Recipe.find(params[:id])
+  erb :"recipes/edit"
+end
+
+# UPDATE
+put '/recipes/:id' do
+  @recipe = Recipe.find(params[:id])
+  if @recipe.update(params[:recipe])
+    redirect "/recipes/#{@recipe.id}"
+  else
+    erb :"recipes/show"
+  end
+end
