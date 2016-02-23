cb0be64db306a1245666fdfee21996b3553b9421
carmela121 <mcarmencarmona@hotmail.com>
2016-02-23 16:48:41 +0000

    Setting up sessions

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -9,3 +9,15 @@ post '/register' do
   end
 end
 
+#handle the form
+post '/register' do 
+  @user = User.new(params[:user])
+  if @user.save
+    session[:user_id] = @user.id
+    flash[:success] = "Thanks for registering!"
+    redirect '/users'
+  else
+    erb :'authentications/new'
+  end
+end
+
