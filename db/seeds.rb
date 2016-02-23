7d7ebb129e262d72dd39e2227d0b7055534a4f7e
carmela121 <mcarmencarmona@hotmail.com>
2016-02-23 11:05:19 +0000

    Seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,51 @@ require_relative "../models/user.rb"
 require_relative "../models/recipe.rb"
 require_relative "../models/category.rb"
 
+User.destroy_all
+Recipe.destroy_all
+Category.destroy_all
+
+[
+  { 
+    name: , 
+    photo_url:, 
+    nationality: 
+  },
+  {
+    name: "Snoop Dog",
+    photo_url: "http://trace.tv/wp-content/uploads/2015/06/snoop-dogg.jpg",
+    nationality: "American"
+  }
+].each do |user|
+  User.create!(user)
+end
+
+[
+  { 
+    name: "John Lennon", 
+    photo_url: "http://t2.gstatic.com/images?q=tbn:ANd9GcRIiu_Tkn8HFUCeI0xElCJ6z2I9_LFob9Wx9GyTu7nseMsASMV5Ug", 
+    nationality: "British"
+  },
+  {
+    name: "Snoop Dog",
+    photo_url: "http://trace.tv/wp-content/uploads/2015/06/snoop-dogg.jpg",
+    nationality: "American"
+  }
+].each do |recipe|
+  Recipe.create!(recipe)
+end
+
+[
+  { 
+    name: "John Lennon", 
+    photo_url: "http://t2.gstatic.com/images?q=tbn:ANd9GcRIiu_Tkn8HFUCeI0xElCJ6z2I9_LFob9Wx9GyTu7nseMsASMV5Ug", 
+    nationality: "British"
+  },
+  {
+    name: "Snoop Dog",
+    photo_url: "http://trace.tv/wp-content/uploads/2015/06/snoop-dogg.jpg",
+    nationality: "American"
+  }
+].each do |category|
+  Category.create!(category)
+end
