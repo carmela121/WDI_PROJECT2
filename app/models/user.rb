7b01a9e4c48701c815b1e5bd71d904ba5b6f23bd
carmela121 <mcarmencarmona@hotmail.com>
2016-02-23 14:58:21 +0000

    Created password table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
   has_many :recipes
   has_many :categories, through: :recipes
+  include BCrypt
+  validates :email, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true
+  validates :password, presence: true
+  has_secure_password
 end
