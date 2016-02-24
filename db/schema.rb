f169b0cdf9cf292debb70016f2271ba53bc53353
carmela121 <mcarmencarmona@hotmail.com>
2016-02-24 10:06:24 +0000

    added password to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160222114846) do
+ActiveRecord::Schema.define(version: 20160223144810) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20160222114846) do
     t.string "last_name"
     t.string "email"
     t.string "image"
+    t.string "password_digest"
   end
 
 end
